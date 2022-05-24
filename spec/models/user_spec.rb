600a00db898e895d88f1bdefcbb532b88ec336d2
Queje <jeremyquerne@hotmail.fr>
2022-05-24 12:06:50 +0200

    factory to create user and a test to check user validation model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do 
+    @user = FactoryBot.create(:user)    
+  end
+
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  context "validation of a devise" do
+    it "is valid with valid attributes" do
+      expect(@user).to be_a(User)
+    end
+
+    describe "test a fake email not to check devise" do
+      it { should_not allow_value(@user.email = "fakeemail" ).for(:email) }  
+    end
+
+    describe "test a fake password with less than 6 letters to check devise" do
+      it { should_not allow_value(@user.password = "f"*5).for(:password) }  
+    end
+
+    describe "test a fake password with more than 128 letters to check devise" do
+      it { should_not allow_value(@user.password = "f"*129).for(:password) }  
+    end
+  end
 end
